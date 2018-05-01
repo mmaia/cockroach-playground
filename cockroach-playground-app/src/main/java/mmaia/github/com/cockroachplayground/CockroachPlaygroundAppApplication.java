3aa2c4d6131e3e37b47727639eae4348a96a352d
Marcos Maia <maia.marcos@gmail.com>
2018-05-01 21:36:33 +0200

    adding initial spring boot project

---
+++ cockroach-playground-app/src/main/java/mmaia/github/com/cockroachplayground/CockroachPlaygroundAppApplication.java
@@ -0,0 +1,12 @@
+package mmaia.github.com.cockroachplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockroachPlaygroundAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockroachPlaygroundAppApplication.class, args);
+	}
+}
