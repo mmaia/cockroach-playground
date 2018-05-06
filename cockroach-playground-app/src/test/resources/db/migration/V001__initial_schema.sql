07dd18b32551280871c5be3073f93ac9eafe7790
Marcos Maia <maia.marcos@gmail.com>
2018-05-06 10:03:56 +0200

    updating for reinstalling dev machine

--- cockroach-playground-app/src/test/resources/db/migration/V001__initial_schema.sql
+++ cockroach-playground-app/src/test/resources/db/migration/V001__initial_schema.sql
@@ -1,20 +1,18 @@
-CREATE DATABASE IF NOT EXISTS playground;
-
-CREATE TABLE playground.user(
+CREATE TABLE user(
   id NUMERIC PRIMARY KEY UNIQUE,
   firstname TEXT NOT NULL,
   lastname TEXT,
   email TEXT
 );
 
-CREATE TABLE playground.meal(
+CREATE TABLE meal(
   id NUMERIC PRIMARY KEY UNIQUE,
   dateTime TIMESTAMP DEFAULT now(),
   description TEXT,
   user_id NUMERIC REFERENCES user(id) NOT NULL
 );
 
-CREATE TABLE playground.weight(
+CREATE TABLE weight(
   id NUMERIC PRIMARY KEY UNIQUE,
   dateTime TIMESTAMP DEFAULT now(),
   value FLOAT,
